11fc2e8d3dde54882be60ed7683ccb9d74536231
воробйов александр <avorobeov21@gmail.com>
2023-04-19 16:08:28 +0300

    fix1

--- Homework_number_18/Program.cs
+++ Homework_number_18/Program.cs
@@ -14,8 +14,7 @@ namespace Homework_number_18
             string line = "(()(()))";
             int numberBracketOpen = 0;
             int numberBracketClose = 0;
-            int maxNumberOpenBrackets = 0;
-            int maxNumberCloseBrackets = 0;
+            int maxDepthNesting = 0;
             int evaluationVisas = 0;
 
             foreach (char item in line)
@@ -23,50 +22,44 @@ namespace Homework_number_18
                 if (item == '(')
                 {
                     numberBracketClose = 0;
-                    if (numberBracketOpen >= maxNumberOpenBrackets)
+                   
+                    if (numberBracketOpen >= maxDepthNesting)
                     {
-                        maxNumberOpenBrackets++;
+                        maxDepthNesting++;
                     }
+                 
                     numberBracketOpen++;
                 }
                 else if (item == ')')
                 {
                     numberBracketOpen = 0;
-                    if (numberBracketClose >= maxNumberCloseBrackets)
+                   
+                    if (numberBracketClose >= maxDepthNesting)
                     {
-                        maxNumberCloseBrackets++;
+                        maxDepthNesting++;
                     }
+                  
                     numberBracketClose++;
                 }
-            }
 
-            foreach (char item in line)
-            {
                 if (item == '(')
                 {
                     evaluationVisas++;
                 }
-                else if (item == ')' && evaluationVisas != 0)
+                else if (item == ')')
                 {
                     evaluationVisas--;
                 }
-                else if (item == ')' && evaluationVisas == 0)
+
+                if (evaluationVisas < 0)
                 {
                     isLineCorrect = false;
-                    break;
                 }
             }
 
             if (isLineCorrect != false && evaluationVisas == 0)
             {
-                if (maxNumberOpenBrackets >= maxNumberCloseBrackets)
-                {
-                    Console.WriteLine($"Строка корректная и максимум глубины равняется {maxNumberOpenBrackets}");
-                }
-                else
-                {
-                    Console.WriteLine($"Строка корректная и максимум глубины равняется {maxNumberCloseBrackets}");
-                }
+                Console.WriteLine($"Строка корректная и максимум глубины равняется {maxDepthNesting}");
             }
             else
             {
